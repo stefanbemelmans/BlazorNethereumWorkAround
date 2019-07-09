d8b54df99b91921d842f6c688b13fdca9e2ded58
stackbaxter <stackbaxter@yahoo.com>
2019-07-09 13:09:03 -0500

    updated contracts

--- nt.Client/NftComponents/AssetNftContracts/NftCreatorData/NftCreatorAddress.cs
+++ nt.Client/NftComponents/AssetNftContracts/NftCreatorData/NftCreatorAddress.cs
@@ -4,6 +4,7 @@
     {
 
         public static string NftCreatorRopstenAddress= "0x414bABeEc0a0Faa22D9FdbfaE461ace4c632a020";
+        public static string NftCreatorRinkebyAddress= "0x10DF15C1BD76050E96f04C44cE11a5e5CA0e935e";
 
         public static string Abi = NftCreatorAbi.Abi; 
     }
