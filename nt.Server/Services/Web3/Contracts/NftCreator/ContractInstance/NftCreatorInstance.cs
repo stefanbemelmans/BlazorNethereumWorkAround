629873df898d6d7fa30e1109cce9537ecb8c6534
stackbaxter <stackbaxter@yahoo.com>
2019-07-12 22:42:49 -0500

    Injected web3 doesn't have access to all the methods

--- nt.Server/Services/Web3/Contracts/NftCreator/ContractInstance/NftCreatorInstance.cs
+++ nt.Server/Services/Web3/Contracts/NftCreator/ContractInstance/NftCreatorInstance.cs
@@ -7,6 +7,8 @@ using Nethereum.RPC;
 using Newtonsoft.Json;
 using nt.Shared.Constants;
 using nt.Server.Services.WebThree.Instance;
+using Microsoft.AspNetCore.Components;
+using Nethereum.Contracts;
 
 namespace nt.Server.Services.WebThree.Contracts.NftCreator.ContractInstance
 {
@@ -14,10 +16,13 @@ namespace nt.Server.Services.WebThree.Contracts.NftCreator.ContractInstance
     {
         public NftCreatorInstance()
         {
+            Contract NftCreatorContractInstance = Web3Instance._.Eth. 
+
         }
        static string NftCreatorAbi = reader.ReadToEnd();
        static readonly StreamReader reader = File.OpenText(@"C:\mv\nt\nt.Shared\Constants\ContractConstants\NftCreator\Abi.json;");
-    
+        [Inject]
+        Web3Instance Web3Instance { get; set; }
     }
 
 }
